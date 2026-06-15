40e4efff97512d9014de54318349d98d44f77800
COLLEGELAB-20 <COLLEGELAB-20@BJSB.LAB>
2026-06-15 15:27:03 +0800

    Added Services

---
+++ src/services/StudentService.java
@@ -0,0 +1,27 @@
+package services;
+
+import models.Student;
+import repositories.StudentRepository;
+
+import java.util.List;
+
+public class StudentService {
+
+    private StudentRepository repository;
+
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addStudent(Student student) {
+        repository.save(student);
+    }
+
+    public Student getStudentById(String id) {
+        return repository.findById(id);
+    }
+
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
+}
